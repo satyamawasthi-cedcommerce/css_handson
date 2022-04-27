5c994da001f5841ea8349fdf630608a49d0cdc3d
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-04-27 15:33:42 +0530

    task-js

---
+++ Jsat1.js
@@ -0,0 +1,12 @@
+
+var i=0;
+ 
+function increaseFun()
+{
+document.getElementById("initval").innerHTML = ++i;
+}
+
+function decreaseFun()
+{
+document.getElementById("initval").innerHTML = --i;
+}
