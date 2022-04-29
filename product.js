f67a0566b50921d07a8997401493dd8120858cbb
satyamawasthi-cedcommerce <104122064+satyamawasthi-cedcommerce@users.noreply.github.com>
2022-04-29 11:12:31 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,17 +1,18 @@
-function addProducts()
+function addProductsToCart()
 {
+let id=document.getElementById("prodid").value;
+let nm=document.getElementById("prodnm").value;
+let p=document.getElementById("prodp").value;
 
-pid=document.getElementById("prodid").value;
-pnm=document.getElementById("prodnm").value;
-pp=document.getElementById("prodp").value;
 
   var table = document.getElementById("myTable");
-  var row = table.insertRow(myTable.rows.length-1);
+  
+  var row = table.insertRow(table.rows.length-1);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  var cell3=row.insertcell(2);
-  cell1.innerHTML = prodid;
-  cell2.innerHTML = prodnm;
-  cell3.innerHTML = prodp;
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = id;
+  cell2.innerHTML = nm;
+  cell3.innerHTML = p;
 
 }
