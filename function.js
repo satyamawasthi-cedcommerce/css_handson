bb6b3d9377880ede510feacbf28c6537e79888a3
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-05-04 10:36:26 +0530

    Understanding Case Sensitivity in Variables

--- function.js
+++ function.js
@@ -1,12 +1,10 @@
-// Only change code below this line
-var a;
-a = 5;
-var b;
-b = 10;
-var c;
-c = "I am a"
-// Only change code above this line
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
