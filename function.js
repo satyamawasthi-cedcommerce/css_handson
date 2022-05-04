0e475abfd66b0ef09fc7573d98370c77acca92db
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-05-04 19:12:20 +0530

    Iterate Through an Array with a For Loop

--- function.js
+++ function.js
@@ -1,13 +1,8 @@
-function multiplyAll(arr) {
-    let product = 1;
-    // Only change code below this line
-  for (var i = 0; i < arr.length; i++) {
-      for (var j = 0; j < arr[i].length; j++) {
-        product = product * arr[i][j];
-      }
-    }
-    // Only change code above this line
-    return product;
-  }
-  
-  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+
+// Only change code below this line
