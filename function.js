1164f3e5a07753eee566b6cbca3a87ea023e0734
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-05-04 18:39:29 +0530

    Iterate with Js for loop

--- function.js
+++ function.js
@@ -1,9 +1,9 @@
-// Setup
-const myArray = [];
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
 
-// Only change code below this line.
-let i = 5;
-while(i >= 0) {
+var myArray = [];
+for (var i = 1; i < 6; i++) {
   myArray.push(i);
-  i--;
 }
