029dfeb15f737884280a873c7067bd0c59a0da60
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-05-05 10:20:24 +0530

    Do-qhile loop

--- function.js
+++ function.js
@@ -1,14 +1,7 @@
-function multiplyAll(arr) {
-  var product = 1;
-  // Only change code below this line
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr[i].length; j++) {
-      product = product * arr[i][j];
-    }
-  }
-  // Only change code above this line
-  return product;
-}
+var myArray = [];
+var i = 10;
 
-// Modify values below to test your code
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+do {
+  myArray.push(i);
+  i++;
+} while (i <= 10);
