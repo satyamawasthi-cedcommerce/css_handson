83f25cef8a829ae7f687f8a13bae25331e3a423a
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-05-05 10:23:06 +0530

    Profile Lookup

--- function.js
+++ function.js
@@ -1,10 +1,46 @@
-function sum(arr, n) {
-  // Only change code below this line
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
 
-  if(n <= 0) {
-    return 0;
-  } else {
-    return sum(arr, n - 1) + arr[n - 1];
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
   }
+  return "No such contact";
 }
+
   // Only change code above this line
+
+
+lookUpProfile("Akira", "likes");
