ec0cbac82aa84e41e79db189d93f3ef0e2c5b066
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-05-05 10:19:32 +0530

    Nesting for loops

--- function.js
+++ function.js
@@ -1,14 +1,14 @@
-var ourArray = [];
-
-for (var i = 0; i < 10; i += 2) {
-  ourArray.push(i);
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product = product * arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
 }
 
-// Setup
-var myArray = [];
-
-// Only change code below this line.
-
-for (var i = 1; i < 10; i += 2) {
-  myArray.push(i);
-}
+// Modify values below to test your code
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
