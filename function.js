55348b50d9dfab303afa372689b57159c70bb18b
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-05-05 10:24:15 +0530

    Generate random function

--- function.js
+++ function.js
@@ -1,46 +1,12 @@
-// Setup
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
-
-function lookUpProfile(name, prop) {
-  // Only change code below this line
-  for (let x = 0; x < contacts.length; x++) {
-    if (contacts[x].firstName === name) {
-      if (contacts[x].hasOwnProperty(prop)) {
-        return contacts[x][prop];
-      } else {
-        return "No such property";
-      }
-    }
+function randomFraction() {
+  // Only change code below this line.
+  var result = 0;
+  // Math.random() can generate 0. We don't want to     return a 0,
+  // so keep generating random numbers until we get one     that isn't 0
+  while (result === 0) {
+    result = Math.random();
   }
-  return "No such contact";
+
+  return result;
+  // Only change code above this line.
 }
-
-  // Only change code above this line
-
-
-lookUpProfile("Akira", "likes");
