0909d2737da901295f571b08a3373431f1c98a9e
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-05-05 10:25:32 +0530

    Generate Random Whole Numbers with JavaScript

--- function.js
+++ function.js
@@ -1,12 +1,6 @@
-function randomFraction() {
-  // Only change code below this line.
-  var result = 0;
-  // Math.random() can generate 0. We don't want to     return a 0,
-  // so keep generating random numbers until we get one     that isn't 0
-  while (result === 0) {
-    result = Math.random();
-  }
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
 
-  return result;
-  // Only change code above this line.
+function randomWholeNum() {
+  // Only change code below this line.
+  return Math.floor(Math.random() * 10);
 }
